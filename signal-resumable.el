27cabd1db5b1fed01c755068938425dd93ee5f44
Zoey Llewellyn Hewll <zoeyhewll@gmail.com>
2025-03-26 12:25:23 +0800

    rename internals, move top-level to test

--- signal-resumable.el
+++ signal-resumable.el
@@ -3,16 +3,15 @@
 (defvar *resumes* ()
   "List of resumption points.")
 
-(defun tag-compatible (sig-tag han-tag)
+(defun sr--tag-compatible (sig-tag han-tag)
+  "Returns true if a signal with tag SIG-TAG can be caught by a handler with tag HAN-TAG."
   ;; todo: typecase rather than symbol equality
   (eq han-tag sig-tag))
 
 (defun signal-resumable (tag data)
-  (message "current handlers: %s" *handlers*)
   (cl-mapl (lambda (tail)
 	     (let ((han (car tail)))
-	       (when (tag-compatible tag (car han))
-		 (message "found compatible handler %s, running %s" han (cdr han))
+	       (when (sr--tag-compatible tag (car han))
 		 ;; ensure handlers are invisible to themselves when run
 		 (dlet ((*handlers* (cdr tail)))
 		   (apply (cdr han) tag data)))))
@@ -21,45 +20,48 @@
   (signal tag data))
 
 (defun resume-from (res-name &rest data)
-  (message "current resumes: %s" *resumes*)
   (when-let ((res (assoc res-name *resumes*)))
-    (message "found compatible restart %s, running %s" res (cdr res))
     (apply (cdr res) data)))
 
 (defmacro handler-bind (handlers &rest body)
-  `(dprepend ((list ,@(mapcar (pcase-lambda (`(,tag ,fn))
+  "Run BODY with the given HANDLERS in scope."
+  `(sr--dprepend ((list ,@(mapcar (pcase-lambda (`(,tag ,fn))
 			 `(cons ',tag ,fn))
 		       handlers))
 	      *handlers*)
      ,@body))
 
 (defmacro restart-bind (restarts &rest body)
-  `(dprepend ((list ,@(mapcar (pcase-lambda (`(,tag ,fn))
+  "Run BODY with the given RESTARTS in scope."
+  `(sr--dprepend ((list ,@(mapcar (pcase-lambda (`(,tag ,fn))
 			 `(cons ',tag ,fn))
 		       restarts))
 	      *resumes*)
      ,@body))
 
-(cl-defmacro dprepend ((list place) &rest body)
+(cl-defmacro sr--dprepend ((list place) &rest body)
   "Prepend LIST to the dynamic PLACE, but only within the dynamic extend of BODY. It is reset on exit."
   (declare (indent 1))
   `(dlet ((,place (append ,list ,place)))
      ,@body))
 
-(cl-defmacro dpush ((val place) &rest body)
+(cl-defmacro sr--dpush ((val place) &rest body)
   "Push VAL to the dynamic PLACE, but only within the dynamic extend of BODY. It is reset on exit."
   (declare (indent 1))
   `(dlet ((,place (cons ,val ,place)))
      ,@body))
 
-(cl-defun error-thing ()
-  (restart-bind ((try-again (lambda (num)
-			      (cl-return-from error-thing
-				(+ 1 num)))))
-    (signal-resumable 'error '(68))))
-
-(handler-bind
-    ((error (lambda (_sig data)
-	      (resume-from 'try-again (+ data 42000)))))
-  (error-thing))
-
+(ert-deftest basic-restart ()
+  "test a basci case of signal handling and restarting."
+  (should
+   (eq 42069
+       (cl-labels ((throws-resumes (val handler)
+		     (cl-block throws-resumes
+		       (restart-bind ((try-again #'(lambda (num)
+						   (cl-return-from throws-resumes
+						     (funcall handler num)))))
+				     (signal-resumable 'error `(,val))))))
+	 (handler-bind
+	     ((error (lambda (_sig data)
+		       (resume-from 'try-again (+ data 42000)))))
+	   (throws-resumes 68 (lambda (v) (+ 1 v))))))))
